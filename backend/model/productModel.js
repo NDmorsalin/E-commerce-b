8dce0712d67be75484cad5895deb7fc68bed89b2
NDmorsalin <morsalin056@gmail.com>
2022-06-29 21:10:35 +0600

    file structer update

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -69,7 +69,7 @@ const productSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
-        required: true,
+        // required: true,
     },
     createdAt: {
         type: Date,
